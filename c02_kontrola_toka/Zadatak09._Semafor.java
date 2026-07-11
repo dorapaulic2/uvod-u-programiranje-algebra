e10078d2ddf8f2c2aec4d7f8312c3b6c09742fb1
dorapaulic2 <dora.paulic2@hotmail.com>
2026-07-11 22:42:39 +0200

    Create Zadatak09._Semafor.java

---
+++ c02_kontrola_toka/Zadatak09._Semafor.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String... args) {
+   for (int i =1; i<=15; i++){
+       if( i % 3 == 1){
+           System.out.println("Crveno");
+       }
+       else if (i % 3 == 2){
+           System.out.println("Žuto");
+       }
+       else {
+           System.out.println("Zeleno");
+       }
+
+   }
+  }
+ }
