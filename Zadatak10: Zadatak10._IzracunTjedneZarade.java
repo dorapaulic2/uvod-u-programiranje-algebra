b2d7b88ea9be04ea1dd3f8f475500214b6fe8a0c
dorapaulic2 <dora.paulic2@hotmail.com>
2026-07-12 18:49:30 +0200

    Create  Zadatak10: Zadatak10._IzracunTjedneZarade.java

---
+++ Zadatak10: Zadatak10._IzracunTjedneZarade.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String... args) {
+     int novac =0;
+     int dan=1;
+     while (dan <=7) {
+         if (dan == 7) {
+             novac = novac + 20;
+         }
+         else {
+             novac = novac + 10;
+         }
+         System.out.println("Nakon " + dan + " dana " + " imam " + novac + " EUR");
+         dan++;
+         
+     }
+    }
+}
